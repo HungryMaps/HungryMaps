741eceab69a12579fdb52c5886c67f28e3672556
laura <lau3093@gmail.com>
2014-05-21 02:08:11 -0600

    Ana-Joan Agregacion de la busqueda por provincia

--- app/models/provin.rb
+++ app/models/provin.rb
@@ -5,4 +5,14 @@ class Provin < ActiveRecord::Base
 	validates_presence_of :direccion_id, message: "Indique un id"
   	validates :direccion_id, presence: true
 	validates_presence_of :nombre_provincia, message: "Indique un nombre"
+def self.search(search)
+
+ if search 
+
+    where('nombre_provincia LIKE ?', "%#{search}%")    
+
+  end
+
+end
+
 end
