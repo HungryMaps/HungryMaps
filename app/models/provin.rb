976cef7f7358684f9b3a83bb0e8b47ce163cad74
DavidRG <darg19920214@gmail.com>
2014-05-21 08:24:56 -0600

    Checkeo

--- app/models/provin.rb
+++ app/models/provin.rb
@@ -3,7 +3,7 @@ class Provin < ActiveRecord::Base
 	has_many :distritos, through: :cantons
 	belongs_to :direccion
 	validates_presence_of :direccion_id, message: "Indique un id"
-  	validates :direccion_id, presence: true
+  	validates :direccion, presence: true
 	validates_presence_of :nombre_provincia, message: "Indique un nombre"
 def self.search(search)
 
